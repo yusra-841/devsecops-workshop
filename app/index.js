78ad85d832d070750cc9b4137594dca3804e2490
abdulwasay475o <f2023266932@umt.edu.pk>
2026-02-11 16:32:38 +0000

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('DevSecOps Workshop Working!'));
+app.listen(3000, () => console.log('Running on port 3000'));
