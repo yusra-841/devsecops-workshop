3922c61b1ed85293d989b4be819f1175abba0504
abdulwasay475o <f2023266932@umt.edu.pk>
2026-02-11 18:08:20 +0000

    feat: demo trigger

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('DevSecOps Workshop Working!'));
+app.get('/', (req, res) => res.send('DevSecOps Working successfully!'));
 app.listen(3000, () => console.log('Running on port 3000'));
